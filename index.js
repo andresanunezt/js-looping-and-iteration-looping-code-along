2c15db13607e9eb2dc192b03aa093859bc467ad4
Andrés Nuñez <andresanunez98@gmail.com>
2021-05-24 22:26:07 -0400

    commit tree 2ce8c8765709ef1846c28193e1d30cdb294dea93

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let thankYouList = []
+for (let i = 0; i < names.length; i++ ) {
+    
+  thankYouList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+return thankYouList;
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+      } 
+}
+
